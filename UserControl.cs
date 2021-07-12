8b9863d4e508d1ce69a02a7bfa6a37e67b90b081
Omar Santiago <omarkenosia@gmail.com>
2021-07-11 21:24:44 -0400

    Add files via upload

---
+++ UserControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UserControl : MonoBehaviour
+{
+    public float MoveSpeed = 3.5f;
+    public float TurnSpeed = 120.0f;
+
+    private void Update()
+    {
+        float vert = Input.GetAxis("Vertical");     // -1 .. 1
+        float horz = Input.GetAxis("Horizontal");
+        this.transform.Translate(Vector3.forward * vert * MoveSpeed * Time.deltaTime);
+        this.transform.localRotation *= Quaternion.AngleAxis(horz * TurnSpeed * Time.deltaTime, Vector3.up);
+    }
+}
